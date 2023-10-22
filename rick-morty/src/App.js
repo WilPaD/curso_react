0e3b804142337f13ac3513e47a0829ff35e2859e
Wilbert Padron <wilbertjose10@gmail.com>
2023-10-22 16:48:38 -0600

    Creando el index y la peticion con axios

--- rick-morty/src/App.js
+++ rick-morty/src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
+import imageRickMorty from './img/rick-morty.png'
+import axios from 'axios'
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [character, setCharacter] = useState(null)
+
+  const reqApi = async() => {
+    await axios.get('https://rickandmortyapi.com/api/character')
+      .then(resul => {
+        const { results } = resul.data
+         setCharacter(results)         
+         console.log(results)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className='title'> Rick & Morty </h1>
+        <img src={imageRickMorty} alt='Rick & Morty' className='img-home'/>
+        <button onClick={reqApi} className='btn-search'>Buscar Personaje</button>
       </header>
     </div>
   );
